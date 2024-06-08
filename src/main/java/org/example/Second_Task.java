ad35f281b243cbb4920bd8ea5a6add9cf4641035
fanatiker1990 <59375292+fanatiker1990@users.noreply.github.com>
2024-06-08 13:26:55 +0300

    add comment to second task

--- src/main/java/org/example/Second_Task.java
+++ src/main/java/org/example/Second_Task.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Second_Task {
     public static void main(String[] args) {
-        int num = 238791;
+        int num = 238791;//наше число
         int min = 9;  // Инициализируем min как максимальное возможное значение цифры
         int max = 0;  // Инициализируем max как минимальное возможное значение цифры
 
