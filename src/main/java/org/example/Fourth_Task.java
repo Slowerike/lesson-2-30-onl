0d436237a15531eb2269a8676e463ce1527e8446
Slowerik <fedarchuk.raman@gmail.com>
2024-06-22 14:11:57 +0300

    2 fixes

--- src/main/java/org/example/Fourth_Task.java
+++ src/main/java/org/example/Fourth_Task.java
@@ -3,21 +3,21 @@ package org.example;
 public class Fourth_Task {
     public static void main(String[] args) {
         long number = 2357234835678L;
-        String result = "";
+        long result1=0;
         long numberForLastCycle = 0;
         while (number != 0) {
             long digit = number % 10;
             number /= 10;
             long nextDigit = number % 10;
             if (digit - 1 == nextDigit) {
-                result += digit;
+                result1=digit+10*result1;
                 numberForLastCycle = digit;
             } else {
                 if (numberForLastCycle - 1 == digit) {
-                    result += digit;
+                    result1=digit+10*result1;
                 }
             }
         }
-        System.out.println(result);
+        System.out.println(result1);
     }
 }
